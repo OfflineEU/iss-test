a18cae6506fb4a4acdb92bf873fa44f03e7fb4e8
Offline <sanya132009@gmail.com>
2020-01-31 12:53:05 +0200

    release v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {IssInfo} from './services/iss-info.service';
+import {interval, Subscription} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {Astronauts, CurrentLocation} from '../environments/interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'iss';
+export class AppComponent implements OnInit, OnDestroy {
+
+  @ViewChild('mapContainer', {static: false}) mapRef: ElementRef;
+
+  location: CurrentLocation = {latitude: '0', longitude: '0'};
+  ISSTime: Date;
+  astronauts: Astronauts = {people: [], number: 0};
+  streamLocation$: Subscription;
+  dataReady = false;
+
+  constructor(private ISSInfoService: IssInfo) {
+  }
+
+  ngOnInit() {
+    this.getCurrentPosition();
+    this.streamLocation$ = interval(5000)
+      .pipe(
+        map(() => {
+          this.getCurrentPosition();
+          this.getAstronauts();
+          this.getCurrentTimestamp();
+          setTimeout(() =>
+            this.initMap(+this.location.latitude, +this.location.longitude), 0);
+        })
+      ).subscribe(() => {
+        this.dataReady = true;
+      });
+  }
+
+  ngOnDestroy() {
+    this.streamLocation$.unsubscribe();
+    this.dataReady = false;
+  }
+
+  getCurrentPosition() {
+    this.ISSInfoService.getLocationData().subscribe(data => {
+      this.location = {...data['iss_position']};
+    });
+  }
+
+  getCurrentTimestamp() {
+    this.ISSInfoService.getLocationData().subscribe(data => {
+      this.ISSTime = new Date();
+      this.ISSTime.setTime(data.timestamp * 1000);
+    });
+  }
+
+  getAstronauts() {
+    this.ISSInfoService.getAstronauts().subscribe(data => {
+      let astronautsArr = data.people;
+      astronautsArr = astronautsArr.filter(astr => astr.craft === 'ISS');
+      this.astronauts = {people: astronautsArr, number: astronautsArr.length};
+    });
+  }
+
+  initMap(latitude, longitude) {
+    const coordinates = {lat: latitude, lng: longitude};
+    const map = new google.maps.Map(
+      this.mapRef.nativeElement, {zoom: 4, center: coordinates});
+    const marker = new google.maps.Marker({position: coordinates, map: map});
+  }
 }
--- src/environments/interface.ts
+++ src/environments/interface.ts
@@ -1,47 +1,10 @@
-export interface Environment {
-  apiKey: string,
-  production: boolean,
-  fbDBUrl: string,
-
+export interface CurrentLocation {
+  longitude: string,
+  latitude: string,
 }
 
-export interface FbAuthResponse {
-  idToken: string,
-  expiresIn: string,
-
+export interface Astronauts {
+  people: [],
+  number: number,
 }
 
-export interface Cameras {
-  'camera-one': boolean,
-  'camera-two': boolean,
-  'camera-three': boolean,
-  'camera-four': boolean,
-}
-
-export interface Staff {
-  'personal number': any,
-  employee: string,
-  arrival: any,
-  leaving: any,
-  done?: any,
-  demand: any,
-}
-
-export interface Fire {
-  'sensor-1': boolean,
-  'sensor-2': boolean,
-  'sensor-3': boolean,
-}
-
-export interface Report {
-  id?: string,
-  camera: number,
-  date: string,
-  time: string,
-  text: string,
-  reportTime?: string,
-}
-
-export interface FbCreateResponse {
-  name: string,
-}
